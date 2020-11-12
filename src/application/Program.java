ce77e8f5caddf5a95f45c41f767c607af54d42a3
HarpiaReal <ev4lente@gmail.com>
2020-11-12 13:49:40 -0300

    Sumário de pedidos mostrado corretamente

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,69 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Client;
+import entities.Order;
+import entities.OrderItem;
+import entities.Product;
+import entities.enums.OrderStatus;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+		
+		int numberOfItems;
+		
+		System.out.println("Enter client data: ");
+		System.out.print("Name: ");
+		String name = sc.nextLine();
+		System.out.print("Email: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		Date birthDate = sdf1.parse(sc.next());
+		
+		Client client = new Client(name, email, birthDate);
+		
+		System.out.println("Enter order Data: ");
+		System.out.print("Status: ");
+		OrderStatus os = OrderStatus.valueOf(sc.next());
+		
+		Order order = new Order(new Date(), os, client);
+		
+		System.out.print("How many items for this order? ");
+		numberOfItems = sc.nextInt();
+		
+		// adds an order to the order list
+		
+		for(int i = 0; i < numberOfItems; i++)
+		{
+			System.out.println("Enter #" + (i + 1) + " item data:");
+			System.out.print("Product Name: ");
+			sc.nextLine();
+			String productName = sc.nextLine();
+			System.out.print("Product Price: ");
+			Double productPrice = sc.nextDouble();
+			System.out.print("Quantity: ");
+			Integer quantity = sc.nextInt();
+			
+			Product product = new Product(productName, productPrice);
+			OrderItem orderItem = new OrderItem(quantity, productPrice, product);
+			order.addItem(orderItem);
+		}
+		
+		System.out.println(order);
+		System.out.printf("Total price: $ %,.2f%n", order.total());
+		
+		sc.close();
 	}
 
 }
